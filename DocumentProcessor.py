c8176778bbe8480e641860148b05818f131077aa
matisse <mail@johex.pro>
2021-12-16 17:53:47 +0100

    soort vna beetje af misschien

--- DocumentProcessor.py
+++ DocumentProcessor.py
@@ -3,6 +3,7 @@ try:
 except ImportError:
     import Image
 import pytesseract
+import re
 
 class DocumentProcessor:
 
@@ -10,18 +11,33 @@ class DocumentProcessor:
         ocrText = pytesseract \
             .image_to_string(Image.open(filename)) \
             .split('\n')
-        _lineWithContractNumber = self.__findLineContractNumber(ocrText)
-        _contractNumber =  self.__filterContractNumberFromLine(_lineWithContractNumber)
+        # _lineWithContractNumber = self.__findLineContractNumber(ocrText)
+        _contractNumber = self.__findLineContractNumber(ocrText)
+        # _contractNumber =  self.__filterContractNumberFromLine(_lineWithContractNumber)
+        print(_contractNumber)
         return _contractNumber
 
     def __findLineContractNumber(self,stringList):
         print("fineLine")
+        # Gwn splitten op spaces en dan 2de??? nope want veel lines
+        # regex die kijke voor string van veel cijfers??
+
         for line in stringList:
-            if "Contractnummer" in line:
-                return line
+            contr_number = re.search('[0-9]{5,}', line)
+            # contr_number = re.search('^[a-zA-Z]{1,4}[0-9]+$', line)
+            if contr_number:
+                # print(contr_number.group())
+                words = line.split(' ')
+                indices = [i for i, s in enumerate(words) if contr_number.group() in s]
+                result = words[indices[0]].replace('.', '')
+                return result
         return "not found"
 
     def __filterContractNumberFromLine(self,line):
         line = line.replace("Contractnummer", "")
         line = line.strip()
         return line
+
+
+# dp = DocumentProcessor()
+# dp.retrieveContractNumber('inputfiles/docu-041.png')
